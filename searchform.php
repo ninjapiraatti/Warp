0474e418fad17966c9385012cf39d6221532aa0f
Ninjapiraatti <tuoppil@gmail.com>
2015-03-01 18:29:03 +0200

    Logo, buttons, forms

---
+++ searchform.php
@@ -0,0 +1,18 @@
+
+
+
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<fieldset>
+		<div class="input-group">
+		    <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="form-control" placeholder="Search for...">
+		  	<span class="input-group-btn">
+				<input type="submit" class="btn btn-default" />
+			</span>
+		</div><!-- /input-group -->
+	</fieldset>
+</form>
+
+
+
+
+
--- single.php
+++ single.php
@@ -45,7 +45,7 @@
 							 * If you want to remove post formats, just delete the post-formats folder and
 							 * replace the function below with the contents of the "format.php" file.
 							*/
-							get_template_part( 'post-formats/format', get_post_format() );
+							get_template_part( 'post-formats/format-single', get_post_format() );
 						?>
 
 					<?php endwhile; ?>
@@ -66,9 +66,11 @@
 
 					<?php endif; ?>
 
+					<?php get_sidebar(); ?>
+
 				</div>
 
-				<?php get_sidebar(); ?>
+				
 
 			</section>
 
