38574cd1a56f33fe9de39cb1e17e07f633d692f0
Ninjapiraatti <tuoppil@gmail.com>
2015-10-17 08:24:41 +0300

    Migrated to gulp.

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+
+var clean           = require('gulp-clean');
+var rename          = require('gulp-rename');
+var jshint          = require('gulp-jshint');
+var concat          = require('gulp-concat');
+var uglify          = require('gulp-uglify');
+var sass            = require('gulp-sass');
+var autoprefixer    = require('gulp-autoprefixer');
+
+
+gulp.task('clean', function() {
+    return gulp.src('library', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('css', function() {
+    return gulp.src('src/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('library'));
+});
+
+
+gulp.task('lint', function() {
+    return gulp.src(['src/**/*.js', '!src/**/*-min.js', '!src/**/picturefill.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('js', function() {
+    var jsDest = 'library/js';
+
+    return gulp.src('src/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(jsDest))
+        .pipe(rename({
+            suffix: '-min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.scss', ['css']);
+    gulp.watch('src/**/*.js', ['lint', 'js']);
+});
+
+gulp.task('default', ['css', 'lint', 'js', 'watch']);
